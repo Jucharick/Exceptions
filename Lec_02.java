42b1eed67bebcab6e6d149e14c5cf05b88ef227b
Julia Ch <juliecha@yandex.ru>
2023-07-04 11:31:25 +0300

    UPD Lec_02

--- Lec_02.java
+++ Lec_02.java
@@ -14,10 +14,35 @@
 // # ClassNotFoundException Класс не найден
 // # UnsupportedOperationException Попытка выполнения нереализованного метода
 
+// 1. Exception — это ошибка, которую можно «на лету» перехватит и
+// исправить.
+// 2. Error — это критическая (непоправимая) ошибка, которую невозможно
+// обработать, и которая приведёт к завершению работы JVM.
 
 
 public class Lec_02 {
   public static void main(String[] args) {
-    
+    // ArithmeticException  -  / by zero
+    int a = 0;
+    int b = 10;
+
+    int c=b/a;
+    System.out.println(c);
+
+  // NullPointerException
+  String str = null;
+  //System.out.println(str.length());
+
+  // ArrayIndexOutOfBoundsException
+  int[] array = new int[10];
+  // array[100] = 5;
+
+  // NumberFormatException
+  // int value = Integer.parseInt (“100a0") ;
+
+  // ClassCastException
+  // Animal animal = new Cat();
+  // Dog dog = (Dog) animal;
+
   }
 }
