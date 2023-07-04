76586c56c6c6613bd3110ad13f4bf71941c1b37e
Julia Ch <juliecha@yandex.ru>
2023-07-04 10:57:14 +0300

    Add Lec_02

---
+++ Lec_02.java
@@ -0,0 +1,23 @@
+// Отказоустойчивые приложения — приложения, которые при
+// возникновении некритичных ошибок могут продолжать свою работу
+
+// Тип исключения:
+// # ArithmeticException Арифметическая ошибка
+// # ArrayIndexOutOfВoundsException Выход индекса за пределы массива
+// # ClassCastException Неверное приведение типов
+// # IllegalArgumentException Употребление недопустимого аргумента при вызове метода
+// # IndexOutOfВoundsException Выход индекса некоторого типа за допустимые пределы
+// # NullPointerException Обращение к несуществующему объекту
+// # NumberFormatException Неверное преобразование символьной строки в числовой формат
+// # IOException Ошибка при работе с потоками ввода/вывода
+// # FileNotFoundException Файл не найден
+// # ClassNotFoundException Класс не найден
+// # UnsupportedOperationException Попытка выполнения нереализованного метода
+
+
+
+public class Lec_02 {
+  public static void main(String[] args) {
+    
+  }
+}
